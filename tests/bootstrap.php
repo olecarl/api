1a23fdc93934960b89cfbc9095dcc5e67f25f91f
Ole Carl <code@olecarl.de>
2025-12-21 12:09:34 +0100

    chore: added phpstan for static code analyse

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,9 +13,9 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-if (method_exists(Dotenv::class, 'bootEnv')) {
+// if (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-}
+// }
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
