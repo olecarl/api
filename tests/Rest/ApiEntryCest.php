01fa4bd23b113f85097513666204ab0dbb976898
Ole Carl <code@olecarl.de>
2026-03-06 06:59:39 +0100

    chore: test suite extended

--- tests/Rest/ApiEntryCest.php
+++ tests/Rest/ApiEntryCest.php
@@ -14,6 +14,8 @@ declare(strict_types=1);
 namespace App\Tests\Rest;
 
 use App\Tests\Support\RestTester;
+use Codeception\Attribute\DataProvider;
+use Codeception\Example;
 
 final class ApiEntryCest
 {
@@ -29,5 +31,23 @@ final class ApiEntryCest
         $I->expect('valid json response');
         $I->seeResponseCodeIsSuccessful();
         $I->seeResponseIsJson();
+    }
+
+    #[DataProvider('formatProvider')]
+    public function trySupportedFormats(RestTester $I, Example $example): void
+    {
+        $I->am('API_USER');
+        $I->expect('content type is "'.$example['accept'].'"');
+        $I->haveHttpHeader('accept', $example['accept']);
+        $I->send('GET', '/');
+        $I->seeResponseCodeIsSuccessful();
+    }
+
+    private function formatProvider(): array
+    {
+        return [
+            ['accept' => 'application/ld+json'],
+            ['accept' => 'application/json'],
+        ];
     }
 }
