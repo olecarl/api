f1f252be4d729aa5546be3cd4ed3ecae764f6c70
Ole Carl <code@olecarl.de>
2026-04-03 23:34:20 +0200

    chore: extended test suite

--- tests/Unit/Entity/UserTest.php
+++ tests/Unit/Entity/UserTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Tests\Unit\Entity;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use App\Tests\Support\UnitTester;
 use Codeception\Test\Unit;
 
@@ -20,10 +21,27 @@ class UserTest extends Unit
     {
     }
 
-    public function testValidUser(): void
+    public function testCreateUser(): void
     {
         $user = User::create(self::EMAIL, self::PASSWORD);
         $this->assertSame(User::ROLE_USER, $user->getRoles()[0]);
         $this->assertFalse($user->isVerified());
+    }
+
+    public function testSaveValidUser(): void
+    {
+        $user = new User();
+        $user->setEmail(self::EMAIL);
+        $container = $this->getModule('Symfony')->_getContainer();
+        $passwordHasher = $container->get('security.password_hasher');
+        $password = $passwordHasher->hashPassword($user, self::PASSWORD);
+        $user->setPassword($password);
+        $users = $container->get(UserRepository::class);
+        $users->save($user);
+
+        $this->assertNotEmpty($user->getId());
+        $this->assertEquals(self::EMAIL, $user->getEmail());
+        $this->assertNotEquals(self::PASSWORD, $user->getPassword());
+        $this->tester->seeInRepository(User::class, ['email' => self::EMAIL]);
     }
 }
