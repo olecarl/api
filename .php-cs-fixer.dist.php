29d1f784a389f2bcf51b3a174bbfb9b6cfdd4024
Ole Carl <code@olecarl.de>
2026-04-03 03:37:10 +0200

    chore: extended code style rules

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -26,6 +26,7 @@ return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
         '@Symfony:risky' => true,
+        '@PHP8x5Migration:risky' => true,
         'linebreak_after_opening_tag' => true,
         'mb_str_functions' => true,
         'no_php4_constructor' => true,
