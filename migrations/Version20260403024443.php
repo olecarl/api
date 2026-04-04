7dda0dbeb1fa171b184e0101b20d4d36760fdb8e
Ole Carl <code@olecarl.de>
2026-04-04 04:38:27 +0200

    chore: added timestampable behaviour

--- migrations/Version20260403024443.php
+++ migrations/Version20260403024443.php
@@ -22,6 +22,8 @@ final class Version20260403024443 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE "user" (id UUID NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, is_verified BOOLEAN NOT NULL, PRIMARY KEY (id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL ON "user" (email)');
+        $this->addSql('ALTER TABLE "user" ADD created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL');
+        $this->addSql('ALTER TABLE "user" ADD updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL');
     }
 
     public function down(Schema $schema): void
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -14,6 +14,8 @@ use ApiPlatform\Metadata\Put;
 use App\Repository\UserRepository;
 use App\State\Processor\UserPasswordHasher;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\Timestampable;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 use Symfony\Bridge\Doctrine\Types\UuidType;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
@@ -42,6 +44,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     public const string ROLE_USER = 'ROLE_USER';
     public const string ROLE_ADMIN = 'ROLE_ADMIN';
+
+    use TimestampableEntity;
 
     #[Groups(['user:read'])]
     #[ORM\Id]
