862268960434b2ad1cfa8d19c691275b7e00b3f7
Ole Carl <code@olecarl.de>
2026-04-18 10:21:03 +0200

    fix: pipeline

--- importmap.php
+++ importmap.php
@@ -16,23 +16,4 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
-    '@hotwired/stimulus' => [
-        'version' => '3.2.2',
-    ],
-    '@hotwired/turbo' => [
-        'version' => '7.3.0',
-    ],
-    'stimulus' => [
-        'version' => '3.2.2',
-    ],
-    'bootstrap' => [
-        'version' => '5.3.8',
-    ],
-    '@popperjs/core' => [
-        'version' => '2.11.8',
-    ],
-    'bootstrap/dist/css/bootstrap.min.css' => [
-        'version' => '5.3.8',
-        'type' => 'css',
-    ],
 ];
